d6814e660d9f4b3a9bc4916ad1014c3f4141a0a7
Icedragal <icedragal@gmail.com>
2022-11-13 12:36:36 +0300

    Задание 1.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
+        //Task 1.1
+        System.out.println("Задание 1.1");
+        int monthlyFee = 15_000;
+        int total = 0;
+        int i = 1;
+        while (total < 2_459_000) {
+            total = total + total / 100;
+            total = total + monthlyFee;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
+            i++;
+        }
     }
 }
